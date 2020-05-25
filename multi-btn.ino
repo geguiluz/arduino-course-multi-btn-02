2adb144efc206c6453448107324b591adb8a5731
geguiluz <memo.eguiluz@gmail.com>
2020-05-24 21:57:47 -0500

    First commit. Works kinda funky

---
+++ multi-btn.ino
@@ -0,0 +1,71 @@
+// Length in miliseconds
+float pressLength = 0;
+
+// Minimum ammount of time for each option
+// Instant press
+int optionOne = 100;
+// Two second press
+int optionTwo = 2000;
+
+int buttonPin = 2;
+
+int greenLed = 12;
+int redLed = 13;
+
+void setup()
+{
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void toggleLed(int ledPin)
+{
+  if (digitalRead(ledPin) == HIGH)
+  {
+    Serial.print("LED is ON, let's turn it OFF");
+    Serial.print('\n');
+    digitalWrite(ledPin, LOW);
+  }
+  else
+  {
+    Serial.print("LED is OFF, let's turn it ON");
+    Serial.print('\n');
+    digitalWrite(ledPin, HIGH);
+  }
+}
+
+void loop()
+{
+  while (digitalRead(buttonPin) == LOW)
+  {
+    delay(100); // Lower this number for higher resolution
+    pressLength = pressLength + 100;
+    if (pressLength >= optionOne)
+    {
+      Serial.print("Toggling both LEDs");
+      Serial.print('\n');
+      toggleLed(greenLed);
+      toggleLed(redLed);
+      break;
+    }
+
+    /*
+    if (pressLength >= optionTwo)
+    {
+      Serial.print("Toggling green LED");
+      toggleLed(greenLed);
+    }
+    else if (pressLength >= optionOne)
+    {
+      Serial.print("Toggling red LED");
+      toggleLed(redLed);
+    }
+    */
+    // Reset pressLength
+    pressLength = 0;
+  }
+}
